b6eaf76a094d631cf291ba8f0933214fe1fffa19
Tory Clasen <ToryClasen@gmail.com>
2021-01-10 17:01:26 -0500

    Add demo app

---
+++ demoapp/src/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True, reload=True)
+
+app = FastAPI()
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+db = [
+    Person(name="Tory", age=32),
+    Person(name="Alex", age=35),
+]
+
+@app.get("/persons/{name}")
+async def read_person(name: str) -> Person:
+    for person in db:
+        if person.name == name:
+            return person
